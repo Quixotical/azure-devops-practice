c5df7ef39309f50f7ee5069f23dccebfc05c742c
Quixotical <jcboomgaarden>
2022-08-15 17:22:07 +0300

    move tf execution to pipeline

--- main.tf
+++ main.tf
@@ -1,41 +1,47 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_res_blobstore"
+    storage_account_name = "quixoticalterraformstate"
+    container_name       = "tfstate"
+    key                  = "terraform.state"
+  }
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.15.0"
+      source  = "hashicorp/azurerm"
+      version = "3.15.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
 }
 
 resource "azurerm_resource_group" "test_group" {
-    name = "tfresgroup"
-    location = "switzerlandnorth"
+  name     = "tfresgroup"
+  location = "switzerlandnorth"
 }
 
 resource "azurerm_container_group" "tfcg_test" {
-    name = "weatherapi"
-    location = azurerm_resource_group.test_group.location
-    resource_group_name = azurerm_resource_group.test_group.name
+  name                = "weatherapi"
+  location            = azurerm_resource_group.test_group.location
+  resource_group_name = azurerm_resource_group.test_group.name
 
-    ip_address_type = "Public"
-    dns_name_label = "quixoticalweatherapi"
-    os_type = "Linux"
+  ip_address_type = "Public"
+  dns_name_label  = "quixoticalweatherapi"
+  os_type         = "Linux"
 
-    container {
-        name = "weatherapi"
-        image = "quixotical/weatherapi"
-        cpu = "1"
-        memory = "1"
+  container {
+    name   = "weatherapi"
+    image  = "quixotical/weatherapi"
+    cpu    = "1"
+    memory = "1"
 
-        ports {
-          port = 80
-          protocol = "TCP"
-        }
+    ports {
+      port     = 80
+      protocol = "TCP"
     }
+  }
 }

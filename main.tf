8ef6551b5c89ca370fffa1f22fb7a6e6b871c5b5
Quixotical <jcboomgaarden>
2022-08-15 16:02:40 +0300

    update gitignore and add main tf file

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.15.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "test_group" {
+    name = "tfresgroup"
+    location = "switzerlandnorth"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name = "weatherapi"
+    location = azurerm_resource_group.test_group.location
+    resource_group_name = azurerm_resource_group.test_group.name
+
+    ip_address_type = "Public"
+    dns_name_label = "quixoticalweatherapi"
+    os_type = "Linux"
+
+    container {
+        name = "weatherapi"
+        image = "quixotical/weatherapi"
+        cpu = "1"
+        memory = "1"
+
+        ports {
+          port = 80
+          protocol = "TCP"
+        }
+    }
+}
